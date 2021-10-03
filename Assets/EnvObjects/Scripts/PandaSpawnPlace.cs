f99165f9bc3b132ca3bf55b16e157ac83a738c8f
gsviripa <gsviripa@tula.co>
2021-10-03 21:02:56 +0300

    update spawn place animation

--- Assets/EnvObjects/Scripts/PandaSpawnPlace.cs
+++ Assets/EnvObjects/Scripts/PandaSpawnPlace.cs
@@ -6,31 +6,43 @@ public class PandaSpawnPlace : MonoBehaviour
 {
     public GameObject pandaPrefab;
     public int pandaCount = 10;
-    public float spawnPeriod = 0.1f;
+    public float spawnPeriod = 5f;
 
     private float time = 0.0f;
-    private BoxCollider2D rb;
     private int currentCount = 0;
+    public Animator animator;
 
     void Start()
     {
-        rb = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         time += Time.deltaTime;
+        if(time < spawnPeriod - 1f || currentCount >= pandaCount)
+        {
+            animator.SetBool("is_spawning", false);
+        } else
+        {
+            animator.SetBool("is_spawning", true);
+        }
 
         if (time >= spawnPeriod && currentCount <= pandaCount)
         {
             time = 0.0f;
-            Vector2 v = rb.transform.position;
-            v.x += 1;
-            Instantiate(pandaPrefab, v, Quaternion.identity);
-            currentCount++;
+            Spawn();
+
         }
         
+    }
+
+    private void Spawn()
+    {
+        Vector2 v = transform.position;
+        v.x += 1;
+        Instantiate(pandaPrefab, v, Quaternion.identity);
+        currentCount++;
     }
 
     
