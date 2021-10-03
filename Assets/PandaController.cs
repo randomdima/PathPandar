a50e0b189bd26e2b9722c2b78a1020e8c2353299
alex <servalex1@gmail.com>
2021-10-03 18:51:59 +0300

    added animations

--- Assets/PandaController.cs
+++ Assets/PandaController.cs
@@ -11,6 +11,7 @@ public class PandaController : MonoBehaviour
 		Regular,
 		Builder,
 		Chef,
+		Digger,
 		Angry
 	}
 
@@ -21,16 +22,34 @@ public class PandaController : MonoBehaviour
 		ExecutingAction
 	}
 
+	public Transform topRightCheck;
+	public Transform bottomRightCheck;
+	public Transform topLeftCheck;
+	public Transform bottomLeftCheck;
+	public Transform ceilingCheck;
+	public Transform groundCheck;
+	public LayerMask m_whatIsGround;
+
+	public Animator animator;
+	
+	public bool facingRight;
 	public float speed;
 	public Rigidbody2D rb;
 	public PandaMode mode;
 	public Activity activity;
+
+
+	public float offsetBottom = 0.5f;
+	public float offsetTop = 0.5f;
+	public float offsetSide = 0.5f;
+	public float rayColisionDistance = 0.3f;
 
 	// Start is called before the first frame update
 	void Start()
 	{
 		activity = Activity.WalkingRight;
 		mode = PandaMode.Regular;
+		//walk(1);
 	}
 
 	// Update is called once per frame
@@ -49,39 +68,142 @@ public class PandaController : MonoBehaviour
 				walk(-1);
 			}
 		}
+
+		SetAnimatorActivitiAndMode();
 	}
 
 	void walk(int d)
 	{
-		rb.AddForce(Vector2.right*speed*d);
+		//rb.AddForce(Vector2.right*speed*d);
+
+		Vector3 targetVelocity = new Vector2(speed * d, rb.velocity.y);
+		// And then smoothing it out and applying it to the character
+		Vector3 curVelocity;
+		rb.velocity = targetVelocity;
+		//rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref curVelocity, 0.05f);
 	}
 
 	private void SetCorrectWalkDirection()
 	{
+		//bool rightObstructed = ObstructedRight();
+		//bool leftObstructed = ObstructedLeft();
+
 		bool rightObstructed = Obstructed(rb.position, Vector2.right);
 		bool leftObstructed = Obstructed(rb.position, Vector2.left);
 
 		if (activity == Activity.WalkingRight && rightObstructed)
 		{
+			Flip();
 			activity = Activity.WalkingLeft;
 			return;
 		}
 
 		if (activity == Activity.WalkingLeft && leftObstructed)
 		{
+			Flip();
 			activity = Activity.WalkingRight;
 			return;
 		}
 	}
 
+	private void Flip()
+	{
+		Vector3 scale = rb.transform.localScale;
+		scale.x *= -1;
+		rb.transform.localScale = scale;
+	}
+
 	private bool Obstructed(Vector2 position, Vector2 direction)
 	{
-		RaycastHit2D raycastHit2d_top =    Physics2D.Raycast(position + direction*0.5f + Vector2.up*0.5f, direction);
-		RaycastHit2D raycastHit2d_bottom = Physics2D.Raycast(position + direction*0.5f - Vector2.up*0.5f, direction);
+
+		RaycastHit2D raycastHit2d_top =    Physics2D.Raycast(position + direction*offsetSide + Vector2.up*offsetTop,    direction);
+		RaycastHit2D raycastHit2d_bottom = Physics2D.Raycast(position + direction*offsetSide - Vector2.up*offsetBottom, direction);
 
 		return
-			(raycastHit2d_top.transform != null && raycastHit2d_top.distance < 0.3)
+			(raycastHit2d_top.transform != null && raycastHit2d_top.distance < rayColisionDistance)
 			||
-			(raycastHit2d_bottom.transform != null && raycastHit2d_bottom.distance < 0.3);
+			(raycastHit2d_bottom.transform != null && raycastHit2d_bottom.distance < rayColisionDistance);
+	}
+
+	private bool ObstructedRight()
+	{
+		float radius = 0.1f;
+		return
+			Physics2D.OverlapCircle(topRightCheck.position, radius, m_whatIsGround)
+			||
+			Physics2D.OverlapCircle(bottomRightCheck.position, radius, m_whatIsGround);
+	}
+
+	private bool ObstructedLeft()
+	{
+		float radius = 0.1f;
+		return
+			Physics2D.OverlapCircle(topLeftCheck.position, radius, m_whatIsGround)
+			||
+			Physics2D.OverlapCircle(bottomLeftCheck.position, radius, m_whatIsGround);
+	}
+
+	private void SetAnimatorActivitiAndMode()
+	{
+		ResetAnimaator();
+
+		setAnimatorWalking(activity == Activity.WalkingLeft || activity == Activity.WalkingRight);
+
+		switch (mode)
+		{
+			case PandaMode.Angry:
+				setAnimatorAngry(true);
+				break;
+			case PandaMode.Builder:
+				setAnimatorBuilder(true);
+				break;
+			case PandaMode.Chef:
+				setAnimatorChef(true);
+				break;
+			case PandaMode.Regular:
+				setAnimatorRegular(true);
+				break;
+			case PandaMode.Digger:
+				setAnimatorDigger(true);
+				break;
+		}
+	}
+
+	private void ResetAnimaator()
+	{
+		setAnimatorAngry(false);
+		setAnimatorBuilder(false);
+		setAnimatorChef(false);
+		setAnimatorDigger(false);
+		setAnimatorRegular(false);
+		setAnimatorWalking(false);
+	}
+
+	private void setAnimatorAngry(bool value)
+	{
+		animator.SetBool("is_angry", value);
+	}
+
+	private void setAnimatorDigger(bool value)
+	{
+		animator.SetBool("is_digger", value);
+	}
+	private void setAnimatorChef(bool value)
+	{
+		animator.SetBool("is_chef", value);
+	}
+	private void setAnimatorBuilder(bool value)
+	{
+		animator.SetBool("is_builder", value);
+	}
+
+	private void setAnimatorRegular(bool value)
+	{
+		animator.SetBool("is_regular", value);
+	}
+
+	private void setAnimatorWalking(bool value)
+	{
+		animator.SetBool("is_walking", value);
 	}
 }
